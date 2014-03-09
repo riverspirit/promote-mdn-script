1ccf3a2f47732df69bccc585b2be918c1a4fd53c
jsx <saurabh@rebugged.com>
2014-03-09 17:22:40 +0530

    Removed dependancy on jQuery

--- promote-mdn.js
+++ promote-mdn.js
@@ -78,8 +78,6 @@
         '#mdndev': 'irc://irc.mozilla.org/mdndev',
         'mozilla-central': 'https://developer.mozilla.org/en-US/docs/mozilla-central',
         'Mozilla': 'https://www.mozilla.org/'
-
-
     }
 
     var options = {
@@ -89,54 +87,75 @@
     };
 
     window._promote_mdn_options = (window._promote_mdn_options) ? window._promote_mdn_options : {};
-    options = $.extend({}, options, window._promote_mdn_options);
+    options = extend({}, options, window._promote_mdn_options);
 
     var replace_count = 0;
     var re = new RegExp(/<a[^>]*>(.*?)<\/a>/);
 
-    $.each($(options.include_elems.join(', ')), function (i, o) {
-        var text = $(o).html();
+    // For the time being, we are not gonna do anything if querySelectorAll is not available in the browser
+    if ('querySelectorAll' in document) {
+        var elements = document.querySelectorAll(options.include_elems.join(', '));
+        Array.prototype.forEach.call(elements, function(o, i){
+            var text = o.innerHTML;
 
-        if (text.match(/<a[^>]*>(.*?)<\/a>/g) && text.match(/<a[^>]*>(.*?)<\/a>/g).length) {
-            var anchor_count = text.match(/<a[^>]*>(.*?)<\/a>/g).length;
-            var anchors = [];
-            var placeholder;
-            var placeholder_index = 0;
+            if (text.match(/<a[^>]*>(.*?)<\/a>/g) && text.match(/<a[^>]*>(.*?)<\/a>/g).length) {
+                var anchor_count = text.match(/<a[^>]*>(.*?)<\/a>/g).length;
+                var anchors = [];
+                var placeholder;
+                var placeholder_index = 0;
 
-            for (var i = 0; i < anchor_count; i++) {
-                var anchor = re.exec(text);
-                placeholder = '{_m$d$n_repl$ace_' + placeholder_index + '_}';
-                anchors[placeholder] = anchor[0];
-                text = text.replace(re, placeholder);
-                placeholder_index++;
-            }
-
-            // text is now stripped of all hyperlinks
-
-            for (var keyword in dataset) {
-                var keyword_regex = new RegExp(keyword, 'i');
-                if (replace_count <= options.maxlinks) {
-                    if (text.match(keyword_regex)) {
-                        var exact_word = keyword_regex.exec(text);
-                        var link = '<a href="'+ dataset[keyword] + options.tracking_sting +'">' + exact_word + '</a>';
-                        placeholder = '{_m$d$n_repl$ace_' + placeholder_index + '_}';
-                        placeholder_index++;
-                        text = text.replace(exact_word, placeholder);
-                        anchors[placeholder] = link;
-                        delete dataset[keyword];
-                        replace_count++;
-                    }
-                } else {
-                    break;
+                for (var i = 0; i < anchor_count; i++) {
+                    var anchor = re.exec(text);
+                    placeholder = '{_m$d$n_repl$ace_' + placeholder_index + '_}';
+                    anchors[placeholder] = anchor[0];
+                    text = text.replace(re, placeholder);
+                    placeholder_index++;
                 }
-            }
 
-            // Now let's replace placeholders with actual anchor tags, pre-existed ones and new ones.
-            for (var l in anchors) {
-                text = text.replace(l, anchors[l]);
-            }
+                // text is now stripped of all hyperlinks
 
-            $(o).html(text);
+                for (var keyword in dataset) {
+                    var keyword_regex = new RegExp(keyword, 'i');
+                    if (replace_count <= options.maxlinks) {
+                        if (text.match(keyword_regex)) {
+                            var exact_word = keyword_regex.exec(text);
+                            var link = '<a href="'+ dataset[keyword] + options.tracking_sting +'">' + exact_word + '</a>';
+                            placeholder = '{_m$d$n_repl$ace_' + placeholder_index + '_}';
+                            placeholder_index++;
+                            text = text.replace(exact_word, placeholder);
+                            anchors[placeholder] = link;
+                            delete dataset[keyword];
+                            replace_count++;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                // Now let's replace placeholders with actual anchor tags, pre-existed ones and new ones.
+                for (var l in anchors) {
+                    text = text.replace(l, anchors[l]);
+                }
+
+                o.innerHTML = text;
+            }
+        });
+    }
+
+    function extend (out) {
+        out = out || {};
+
+        for (var i = 1; i < arguments.length; i++) {
+            if (!arguments[i])
+                continue;
+
+            for (var key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key))
+                    out[key] = arguments[i][key];
+            }
         }
-    });
+
+        return out;
+    };
+
 })();
